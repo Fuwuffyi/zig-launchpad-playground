6ed3af6322ac9b68206748bdede68bafcbf2ab36
Fuwuffy <palazzini21@gmail.com>
2025-07-08 19:50:22 +0200

    Remove testing movement code

--- src/main.zig
+++ src/main.zig
@@ -8,11 +8,6 @@ pub fn main() !void {
     const allocator: std.mem.Allocator = std.heap.page_allocator;
     // Initialize launchpad
     var launchpad = try Launchpad.init(allocator);
-    // Setup position variables
-    var p_x: u8 = 0;
-    var p_y: u8 = 0;
-    var x: u8 = 0;
-    var y: u8 = 0;
     // Run loop to get messages
     var should_stop: bool = false;
     while (!should_stop) {
@@ -27,24 +22,6 @@ pub fn main() !void {
             if (message.matchesKey(Launchpad.getNamedKey(.VOL))) {
                 try launchpad.clearGridLights();
             }
-            // On arrow keys
-            if (message.matchesKey(Launchpad.getNamedKey(.UP))) {
-                if (y > 0) y -= 1;
-            }
-            if (message.matchesKey(Launchpad.getNamedKey(.DOWN))) {
-                if (y < 7) y += 1;
-            }
-            if (message.matchesKey(Launchpad.getNamedKey(.LEFT))) {
-                if (x > 0) x -= 1;
-            }
-            if (message.matchesKey(Launchpad.getNamedKey(.RIGHT))) {
-                if (x < 7) x += 1;
-            }
-            if (x == p_x and y == p_y) continue;
-            try launchpad.setGridLight(p_x + p_y * 8, 0);
-            try launchpad.setGridLight(x + y * 8, 45);
-            p_y = y;
-            p_x = x;
         }
     }
 }
